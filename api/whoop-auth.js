fe0b95ea394339fa18685365e6b9e01ac36212d1
4th8548yw5-dev <4th8548yw5@privaterelay.appleid.com>
2026-05-28 18:33:09 -0400

    Create whoop-auth.js

---
+++ api/whoop-auth.js
@@ -0,0 +1,9 @@
+export default async function handler(req, res) {
+  const params = new URLSearchParams({
+    client_id: process.env.WHOOP_CLIENT_ID,
+    redirect_uri: process.env.WHOOP_REDIRECT_URI,
+    response_type: 'code',
+    scope: 'read:recovery read:cycles read:sleep read:workout read:profile read:body_measurement'
+  });
+  res.redirect(`https://api.prod.whoop.com/oauth/oauth2/auth?${params}`);
+}
